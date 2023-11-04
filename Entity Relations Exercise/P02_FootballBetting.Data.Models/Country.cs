cfb7a0c6e089763f8d2cdf9233e2899bb680f170
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2023-11-04 11:24:42 +0300

    EFC code first

---
+++ Entity Relations Exercise/P02_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Town> Towns { get; set; }
+    }
+}
---
+++ Entity Relations Exercise/P02_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public virtual ICollection<Bet> Bets { get; set;}
+    }
+}

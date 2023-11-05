2494acce44f934cd177a2f0ad8d27dd5c4b5cf46
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2023-11-05 10:39:28 +0300

    EFC code first

---
+++ Entity Relations Exercise2/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        private const string connectionString = $"Server=DESKTOP-MB8TJ24\\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;";
+
+        public StudentSystemContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+     
+
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+        }
+
+    }
+
+
+    
+}
